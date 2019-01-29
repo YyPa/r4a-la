121baa54e1ee0157605ed81783c5e491876ed181
YyPa <40070476+YyPa@users.noreply.github.com>
2019-01-29 20:34:42 +0300

    Update sondakika.js

---
+++ komutlar/sondakika.js
@@ -0,0 +1,25 @@
+const Discord = require('discord.js');
+exports.run = function(client, message, args) {
+var request = require('request'); // ilk önce kullanacağımız modülü yani request'i belirtelim.
+request('https://simsekapi.glitch.me/HgV6Aca9/sondakika', function (error, response, body) { // veri çekeceğimiz siteyi belirliyoruz.
+    if (error) return message.channel.send('Hata:', error); // hata varsa kanala göndersin.
+    else if (!error) { // hata yoksa;
+        var veri = JSON.parse(body); // siteden ana veriyi çeksin.
+        message.channel.send(veri.sondakika); // çekilen değeri filtreleyerek kanala yollasın.
+    } // eğer kodunu kapatalım.
+}); // en s
+
+};
+
+exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: [],
+  permLevel: 0
+};
+
+exports.help = {
+  name: 'sondakika',
+  description: '',
+  usage: 'sondakika'
+};
