d692b6a7bea9a8a098c58d82630f780e76aab854
YyPa <40070476+YyPa@users.noreply.github.com>
2019-01-29 21:41:05 +0300

    Update emoji.js

--- komutlar/emoji.js
+++ komutlar/emoji.js
@@ -1,4 +1,12 @@
 const Discord = require('discord.js');
+const client = new Discord.Client();
+const ayarlar = require('../ayarlar.json');
+const randomizeCase = word => word.split('').map(c => Math.random() > 0.5 ? c.toUpperCase() : c.toLowerCase()).join('');
+
+var prefix = ayarlar.prefix;
+
+
+
 
 const mapping = {
     ' ': '   ',
@@ -8,6 +16,19 @@ const mapping = {
     '3': ':three:',
     '4': ':four:',
     '5': ':five:',
+    'İ': ':regional_indicator_i:',
+    'Ö': ':regional_indicator_o:',
+    'ö': ':regional_indicator_o:',
+    'Ş': ':regional_indicator_s:',
+    'Ü': ':regional_indicator_u:',
+    'Ç': ':regional_indicator_c:', 
+    'ı': ':regional_indicator_i:', 
+    'o': ':regional_indicator_o:',
+    'ş': ':regional_indicator_s:',
+    'ğ': ':regional_indicator_g:',
+    'Ğ': ':regional_indicator_g:',
+    'ü': ':regional_indicator_u:',
+    'ç': ':regional_indicator_c:', 
     '6': ':six:',
     '7': ':seven:',
     '8': ':eight:',
@@ -19,27 +40,33 @@ const mapping = {
 };
 
 'abcdefghijklmnopqrstuvwxyz'.split('').forEach(c => {
-	mapping[c] = mapping[c.toUpperCase()] = `:regional_indicator_${c}:`;
+    mapping[c] = mapping[c.toUpperCase()] = ` :regional_indicator_${c}:`;
 });
 
+exports.run = (bot, msg, args) => {
+    if (args.length < 1) {
+        throw '**Bir mesaj belirt**';
+    }
 
-exports.run = function(client, message, args) {
-
-	if (args.length < 1) return message.reply('Lütfen bir mesaj belirt. **Doğru Kullanım**: c!emoji <mesaj>')
-		
-	message.channel.send(args.join(' ').split('').map(c => mapping[c] || c).join(' '));
-
+  		if (args.length > 50) return msg.channel.send(`${this.client.emojis.get('509395840821886986')} En fazla 50 Harf Kullanabilirsiniz.`);
+  
+    msg.channel.send(
+        args.join(' ')
+            .split('')
+            .map(c => mapping[c] || c)
+            .join('')
+    );
 };
 
 exports.conf = {
-  enabled: true, 
-  guildOnly: true, 
-  aliases: [],
-  permLevel: 0 
+  enabled: true,
+  guildOnly: false,
+  aliases: ['emojiyazısı', 'emojiyaz', 'emoji-yazı'],
+  permLevel: 0
 };
 
 exports.help = {
-  name: 'emoji', 
-  description: 'Mesajınızı emoji haline getirir',
-  usage: 'emoji <mesaj>'
+  name: 'emojiyazı',
+  description: 'Mesajınızı emojiye çevirir.',
+  usage: 'emojiyazı <mesaj>'
 };
