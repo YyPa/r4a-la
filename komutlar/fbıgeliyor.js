6a04dc3a852faf274d2c563e2769d79737b67e27
YyPa <40070476+YyPa@users.noreply.github.com>
2019-01-29 21:36:59 +0300

    Create fbıgeliyor.js

---
+++ komutlar/fbıgeliyor.js
@@ -0,0 +1,34 @@
+const Discord = require('discord.js');
+const ayarlar = require('../ayarlar.json');
+
+exports.run = (client, message, params) => {
+    if (!message.guild) {
+    const ozelmesajuyari = new Discord.RichEmbed()
+    .setColor('RANDOM')
+    .setTimestamp()
+    .setAuthor(message.author.username, message.author.avatarURL)
+    .addField('**Eğlence Komutları Özel Mesajlarda Kullanılamaz!**')
+    return message.author.sendEmbed(ozelmesajuyari); }
+    if (message.channel.type !== 'dm') {
+      const sunucubilgi = new Discord.RichEmbed()
+    .setAuthor(message.author.username + ' FBI Geliyor!!!!')
+    .setColor('RANDOM')
+    .setTimestamp()
+    .setDescription('')
+        .setImage(`https://media.giphy.com/media/2Wkblpy4kuakM/giphy.gif`)
+    return message.channel.sendEmbed(sunucubilgi);
+    }
+};
+
+exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: [],
+  permLevel: 0
+};
+
+exports.help = {
+  name: 'fbı',
+  description: 'Polisi Arar (ciddiye almayın)',
+  usage: 'fbı'
+};
