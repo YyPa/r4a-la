239b61783af136576c4f555067e512e1c0f16192
YyPa <40070476+YyPa@users.noreply.github.com>
2019-01-29 21:51:31 +0300

    Update cekilis.js

--- komutlar/cekilis.js
+++ komutlar/cekilis.js
@@ -1,24 +1,32 @@
-const Discord = require('discord.js');
+const Discord = require('discord.js')
+
 
 exports.run = (client, message, args) => {
-    const embedçekiliş = new Discord.RichEmbed()
-        .setTitle("Çekiliş Kazananı")
-        .setDescription(`Çekilişi Kazanan: ${message.guild.members.random().displayName}`)
-    console.log("!çekiliş komutu " + message.author.username + " tarafından kullanıldı.")
-    console.log ('!çekiliş komudunu kullanan işinin ismini verim aga' + message.author.username )
+    let mesaj = args.slice(0).join(' ');
+    if (!message.member.hasPermission("ADMINISTRATOR")) return message.reply('Bunun için gerekli iznin yok');
+    if (mesaj.length < 1) return message.reply('Ödülü Yazmayı Unuttun ');
     message.delete();
-    message.channel.sendEmbed(embedçekiliş);
+    const embed = new Discord.RichEmbed()
+    .addField('Sunucu İsmi:', message.guild.name , true)    
+    .setColor('RANDOM')
+    .addField('Ödül', mesaj)
+    .addField("Çekilişi Başlatan:", `<@${message.author.id}>`, true)
+    .addField("Çekilişin Yapıldığı Kanal:", message.channel)
+    .addField("Çekilişin Yapıldığı Zaman:", message.createdAt)
+    .addField('<a:parti:504229366104850432>Çekilişi Kazanan<a:parti:504229366104850432>', `<@${message.guild.members.random().id}>`)
+    .setThumbnail(message.guild.iconURL)
+    return message.channel.sendEmbed(embed);
 };
 
 exports.conf = {
     enabled: true,
     guildOnly: false,
-    aliases: ['çekilişyap', 'çekiliş-yap', 'lottery'],
-    permLevel: 0
-};
-
-exports.help = {
+    aliases: [],
+    permLevel: 3
+  };
+  
+  exports.help = {
     name: 'çekiliş',
-    description: 'Çekiliş yapar.',
-    usage: 'çekiliş'
-};
+    description: 'embedyaz',
+    usage: 'embedyaz'
+  };
